1b8d30843f1235ba6b99ec0d5ab4b8a3a17c71e1
Nik <borovikow.nikita@gmail.com>
2014-05-23 08:13:38 +0400

    Bot upgrade

--- Chat bot/Program.fs
+++ Chat bot/Program.fs
@@ -9,23 +9,40 @@ open elizaBotLibrary
 open System.Windows.Forms
 open System.Drawing
 
-let rec readMsg (msg: string, answer: string) = 
+let rec readMsg = 
     let frm = new Form(Width = 500, Height = 160, Location = Point(900,900))
-    let lbY = new Label(Text = sprintf "You: %A" msg, Width = 300, Top = 10, Height = 20)
-    let lbE = new Label(Text = sprintf "Eliza: %A" answer, Width = 300, Top = 30, Height = 20)
+    let lbY = new Label(Text = sprintf "You: ...", Width = 500, Top = 10, Height = 20)
+    let lbE = new Label(Text = sprintf "Eliza: Enter your message...", Width = 500, Top = 30, Height = 15)
     let tb = new TextBox(Text = " ", Top = 50, Width = 200, Height = 20)
-    let btSend = new Button(Text="Sent", Top = 80, Width = 500, Height = 20)
-    let btExit = new Button(Text="Exit", Top = 100, Left = 211, Height = 20)
-    btSend.Click.Add (fun _ -> let userMsg = tb.Text
-                               frm.Close()
-                               readMsg (userMsg, meliza_response userMsg)
+    let btSend = new Button(Text="Sent", Top = 80, Left = 45, Width = 400, Height = 20)
+    let btBeep = new Button(Text="BEEP", Top = 50, Left = 300, Height = 20)
+    let btExit = new Button(Text="Exit", Top = 100, Left = 207, Height = 20)
+
+    tb.Focus |> ignore
+
+    let answer userMsg =
+        lbY.Text <- "You: " + userMsg 
+        lbE.Text <- "Eliza: " + (meliza_response userMsg)
+        tb.Text <- ""
+    
+    tb.KeyDown.Add (fun key -> if key.KeyCode = Keys.Escape then frm.Close()
+                               else if key.KeyCode = Keys.Enter then 
+                                   answer tb.Text
                                )
-    btExit.Click.Add (fun _ -> frm.Close()
+
+    btSend.Click.Add (fun _ -> answer tb.Text)
+
+    btBeep.Click.Add (fun _ -> System.Console.Beep()
+                               lbE.Text <- " BEEP!! "
+                               btBeep.Hide()
+                               btBeep.Show()
                                )
-    frm.Controls.AddRange [| lbY; lbE; tb; btSend; btExit |]
+
+    btExit.Click.Add (fun _ -> frm.Close())
+    tb.LostFocus.Add (fun _ -> tb.Focus|> ignore)
+    frm.Controls.AddRange [| lbY; lbE; tb; btSend; btExit; btBeep |]
     frm.Show()
 
-
-readMsg("", "Enter your message...")
-
-System.Console.ReadKey() |> ignore
+[<EntryPoint>]
+readMsg
+Application.Run()
